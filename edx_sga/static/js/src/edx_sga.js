4d23f2b22a294ac6dd3b227d1212d4b3217080fa
Chris Rossi <chris@archimedeanco.com>
2014-05-01 10:37:35 -0400

    Make the XBlock javascript work both in Studio and LMS.

--- edx_sga/static/js/src/edx_sga.js
+++ edx_sga/static/js/src/edx_sga.js
@@ -1,6 +1,6 @@
 /* Javascript for StaffGradedAssignmentXBlock. */
 function StaffGradedAssignmentXBlock(runtime, element) {
-    require(["jquery", "underscore", "jquery.fileupload"], function($, _) {
+    function xblock($, _) {
         var uploadUrl = runtime.handlerUrl(element, 'upload_assignment');
         var downloadUrl = runtime.handlerUrl(element, 'download_assignment');
         var template = _.template($(element).find("#sga-tmpl").text());
@@ -11,7 +11,6 @@ function StaffGradedAssignmentXBlock(runtime, element) {
             $(content).find("#fileupload").fileupload({
                 url: uploadUrl,
                 add: function(e, data) {
-                    console.log("hey", data);
                     var do_upload = $(content).find("#do-upload")
                         .text("Upload " + data.files[0].name + " ");
                     $('<button/>')
@@ -35,5 +34,30 @@ function StaffGradedAssignmentXBlock(runtime, element) {
             var state = $(element).find(".sga-block").attr("data-state");
             render(JSON.parse(state));
         });
-    });
+    }
+
+    if (require === undefined) { 
+        /** 
+         * The LMS does not use require.js (although it loads it...) and
+         * does not already load jquery.fileupload.  (It looks like it uses
+         * jquery.ajaxfileupload instead.  But our XBlock uses 
+         * jquery.fileupload.
+         */
+        function loadjs(url) {
+            $("<script>")
+                .attr("type", "text/javascript")
+                .attr("src", url)
+                .appendTo(element);
+        }
+        loadjs("/static/js/vendor/jQuery-File-Upload/js/jquery.iframe-transport.js");
+        loadjs("/static/js/vendor/jQuery-File-Upload/js/jquery.fileupload.js");
+        xblock($, _);
+    }
+    else {
+        /**
+         * Studio, on the other hand, uses require.js and already knows about
+         * jquery.fileupload.
+         */
+        require(["jquery", "underscore", "jquery.fileupload"], xblock);
+    }
 }
