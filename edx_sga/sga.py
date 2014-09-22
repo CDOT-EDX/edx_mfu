1f0e402032f2e7dd2536ea449ca3cd36e48b45a9
Benjamin Snively <wastedelectrons@outlook.com>
2014-09-22 16:18:35 -0400

    Added note to request.user
    added note to request.user

--- edx_sga/sga.py
+++ edx_sga/sga.py
@@ -275,6 +275,7 @@ class StaffGradedAssignmentXBlock(XBlock):
 
         # NEW CODE BEGINS :
 
+        ###There is no user property in a webob.request object.
         studentName = request.user.username
 
         #studentDirectory = 'static/' + studentName + datetime.now().strftime("/%Y/%m/%d/%H/%M/%S")
