c035a77add80eceb275bad693b56b667baa2b247
Benjamin Snively <wastedelectrons@outlook.com>
2014-09-24 15:00:27 -0400

    deploy.sh now restarts lms and cms

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@
 git pull
 sudo -u edxapp /edx/bin/pip.edxapp uninstall -y edx-sga
 sudo -u edxapp /edx/bin/pip.edxapp install edx-sga 
+sudo /edx/bin/supervisorctl -c /edx/etc/supervisord.conf restart edxapp:
