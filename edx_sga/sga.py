08a9af23171cd7f378c6185be260e66154835713
Benjamin Snively <wastedelectrons@outlook.com>
2014-10-20 13:48:13 -0400

    Dict fix

--- edx_sga/sga.py
+++ edx_sga/sga.py
@@ -209,7 +209,7 @@ class StaffGradedAssignmentXBlock(XBlock):
         rendering in client view.
         """
         #if self.uploaded_sha1:
-        if len(self.uploaded_files) != 0:
+        if len(self.uploaded_files.keys()) != 0:
             #uploaded = {"filename": self.uploaded_filename}
             uploaded = None
         else:
