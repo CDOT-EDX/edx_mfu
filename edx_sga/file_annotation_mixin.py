f11e74faa1efa7fbd1ba1cc60263de9896ace42c
Benjamin Snively <wastedelectrons@outlook.com>
2014-11-10 08:52:44 -0500

    indentation to tab

--- edx_sga/file_annotation_mixin.py
+++ edx_sga/file_annotation_mixin.py
@@ -22,72 +22,72 @@ from django.template import Context, Template
 
 class FileAnnotationMixin(XBlockMixin):
 	"""
-    Mixin for handling annotations.
+	Mixin for handling annotations.
 	"""
-    annotated_files = Dict(
-        display_name="Uploaded Files",
-        scope=Scope.user_state,
-        default=dict(),
-        help="Files uploaded by the user. Tuple of filename, mimetype and timestamp"
-    )	
+	annotated_files = Dict(
+		display_name="Uploaded Files",
+		scope=Scope.user_state,
+		default=dict(),
+		help="Files uploaded by the user. Tuple of filename, mimetype and timestamp"
+	)	
 
-    @XBlock.handler
-    def staff_upload_annotation(self, request, suffix=''):
-        self.get_student_state(request.params['module_id'])
-        upload = request.params['assignment']
+	@XBlock.handler
+	def staff_upload_annotation(self, request, suffix=''):
+		self.get_student_state(request.params['module_id'])
+		upload = request.params['assignment']
 
-        return self.upload_file(
-            self.get_student_state(request.params['module_id'], 
-            request.params['assignment']
-        )
-        
-    @XBlock.handler
-    def student_download_annotation(self, request, suffix=''):
-        return self.download_file(self.annotated_files, suffix)
+		return self.upload_file(
+			self.get_student_state(request.params['module_id'], 
+			request.params['assignment']
+		)
+		
+	@XBlock.handler
+	def student_download_annotation(self, request, suffix=''):
+		return self.download_file(self.annotated_files, suffix)
 
-    @XBlock.handler
-    def staff_download_annotation(self, request, suffix=''):
-        return self.download_file(
-            self.get_student_state(request.params['module_id'], 
-            suffix
-        )
-    
-    #For downloading the entire assingment for one student.
-    @XBlock.handler
-    def staff_download_annotation_zipped(self, request, suffix=''):
-        assert self.is_course_staff()
-        module = StudentModule.objects.get(pk=request.params['module_id'])
-        state = json.loads(module.state)
+	@XBlock.handler
+	def staff_download_annotation(self, request, suffix=''):
+		return self.download_file(
+			self.get_student_state(request.params['module_id'], 
+			suffix
+		)
+	
+	#For downloading the entire assingment for one student.
+	@XBlock.handler
+	def staff_download_annotation_zipped(self, request, suffix=''):
+		assert self.is_course_staff()
+		module = StudentModule.objects.get(pk=request.params['module_id'])
+		state = json.loads(module.state)
 
-        #TODO: assignment name with student, course and assignemnt name.
-        #return self.download_zipped(self.annotated_files, 'assignment')
-        return self.download_zipped(
-            state['annotated_files'], 
-            display_name + "-" + username + "annotated.zip";
-        )
+		#TODO: assignment name with student, course and assignemnt name.
+		#return self.download_zipped(self.annotated_files, 'assignment')
+		return self.download_zipped(
+			state['annotated_files'], 
+			display_name + "-" + username + "annotated.zip";
+		)
 
-    @XBlock.handler
-    def student_download_annotation_zipped(self, request, suffix=''):
-        #TODO: assignment name with course and assignemnt name.
-        #return self.download_zipped(self.annotated_files, 'assignment')
-        return self.download_zipped(
-            self.annotated_files, 
-            display_name + "-" + username + "annotated.zip";
-        )
-    @XBlock.handler
-    def staff_delete_annotation(self, request, suffix=''):
-        module_id = request.params['module_id']
-        uploaded = self.get_student_state(module_id)
-        newFilelist = self.delete_file(uploaded, suffix)
-        self.set_student_state(
-            module_id, 
-            annotated_files = newFilelist
-        )
+	@XBlock.handler
+	def student_download_annotation_zipped(self, request, suffix=''):
+		#TODO: assignment name with course and assignemnt name.
+		#return self.download_zipped(self.annotated_files, 'assignment')
+		return self.download_zipped(
+			self.annotated_files, 
+			display_name + "-" + username + "annotated.zip";
+		)
+	@XBlock.handler
+	def staff_delete_annotation(self, request, suffix=''):
+		module_id = request.params['module_id']
+		uploaded = self.get_student_state(module_id)
+		newFilelist = self.delete_file(uploaded, suffix)
+		self.set_student_state(
+			module_id, 
+			annotated_files = newFilelist
+		)
 
-        return Response(status=204)
+		return Response(status=204)
 
-    def annotated_file_list(self, module_id):
-        return get_student_state(module_id)['annotated_files']
+	def annotated_file_list(self, module_id):
+		return get_student_state(module_id)['annotated_files']
 
 def _now():
-    return datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
+	return datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
--- edx_sga/file_submission_mixin.py
+++ edx_sga/file_submission_mixin.py
@@ -24,77 +24,77 @@ class FileSubmissionMixin(XBlockMixin):
 	"""
 	Mixin for handling file submissions.
 	"""
-    uploaded_files = Dict(
-        display_name="Uploaded Files",
-        scope=Scope.user_state,
-        default=dict(),
-        help="Files uploaded by the user. Tuple of filename, mimetype and timestamp"
-    )	
+	uploaded_files = Dict(
+		display_name="Uploaded Files",
+		scope=Scope.user_state,
+		default=dict(),
+		help="Files uploaded by the user. Tuple of filename, mimetype and timestamp"
+	)	
 
-    @XBlock.handler
-    def student_upload_file(self, request, suffix=''):
-        assert self.upload_allowed()
+	@XBlock.handler
+	def student_upload_file(self, request, suffix=''):
+		assert self.upload_allowed()
 
-        return self.upload_file(
-        	self.uploaded_files, 
-        	request.params['assignment']
-        )
-        
-    @XBlock.handler
-    def student_download_file(self, request, suffix=''):
-        return self.download_file(self.uploaded_files, suffix)
+		return self.upload_file(
+			self.uploaded_files, 
+			request.params['assignment']
+		)
+		
+	@XBlock.handler
+	def student_download_file(self, request, suffix=''):
+		return self.download_file(self.uploaded_files, suffix)
 
-    @XBlock.handler
-    def staff_download_file(self, request, suffix=''):
-        return self.download_file(
-        	self.uploaded_file_list(request.params['moudule_id']),
-         	suffix
-         )
-    
-    #For downloading the entire assingment for one student.
-    @XBlock.handler
-    def staff_download_zipped(self, request, suffix=''):
-        #TODO: assignment name with student, course and assignemnt name.
-        return self.download_zipped(
-        	self.uploaded_file_list(request.params['moudule_id']), 
-        	display_name + "-" + module.student.username + ".zip";
-        )
+	@XBlock.handler
+	def staff_download_file(self, request, suffix=''):
+		return self.download_file(
+			self.uploaded_file_list(request.params['moudule_id']),
+			suffix
+		 )
+	
+	#For downloading the entire assingment for one student.
+	@XBlock.handler
+	def staff_download_zipped(self, request, suffix=''):
+		#TODO: assignment name with student, course and assignemnt name.
+		return self.download_zipped(
+			self.uploaded_file_list(request.params['moudule_id']), 
+			display_name + "-" + module.student.username + ".zip";
+		)
 
-    @XBlock.handler
-    def student_download_zipped(self, request, suffix=''):
-        #TODO: assignment name with course and assignemnt name.
-        return self.download_zipped(
-        	self.uploaded_files, 
-        	display_name + "-" + username + ".zip";
-        )
+	@XBlock.handler
+	def student_download_zipped(self, request, suffix=''):
+		#TODO: assignment name with course and assignemnt name.
+		return self.download_zipped(
+			self.uploaded_files, 
+			display_name + "-" + username + ".zip";
+		)
 
-    @XBlock.handler
-    def student_delete_file(self, request, suffix=''):
-        """Removes an uploaded file from the assignemtn
+	@XBlock.handler
+	def student_delete_file(self, request, suffix=''):
+		"""Removes an uploaded file from the assignemtn
 
-        Keyword arguments:
-        request: not used.
-        suffix:  holds the key hash of the file to be deleted.
-        """
-        assert self.upload_allowed()
-        self.delete_file(self.uploaded_files, suffix)
-        return Response(status = 204)
+		Keyword arguments:
+		request: not used.
+		suffix:  holds the key hash of the file to be deleted.
+		"""
+		assert self.upload_allowed()
+		self.delete_file(self.uploaded_files, suffix)
+		return Response(status = 204)
 
-    @XBlock.handler
-    def staff_delete_file(self, request, suffix=''):
-        module_id = request.params['module_id']
-        uploaded = self.get_student_state(module_id).get('uploaded_files')
+	@XBlock.handler
+	def staff_delete_file(self, request, suffix=''):
+		module_id = request.params['module_id']
+		uploaded = self.get_student_state(module_id).get('uploaded_files')
 
-        newFilelist = self.delete_file(uploaded, suffix)
-        self.set_student_state(
-            module_id, 
-            uploaded_files = newFilelist
-        )
+		newFilelist = self.delete_file(uploaded, suffix)
+		self.set_student_state(
+			module_id, 
+			uploaded_files = newFilelist
+		)
 
-        return Response(status=204)
+		return Response(status=204)
 
-    def uploaded_file_list(self, module_id):
-    	return get_student_state(module_id)['uploaded_files']
+	def uploaded_file_list(self, module_id):
+		return get_student_state(module_id)['uploaded_files']
 
 def _now():
-    return datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
+	return datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
