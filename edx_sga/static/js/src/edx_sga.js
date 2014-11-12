341eea4976dd06a0c9b8f293d7d5362fe2a0e066
Benjamin Snively <wastedelectrons@outlook.com>
2014-11-12 14:35:41 -0500

    typo

--- edx_sga/static/js/src/edx_sga.js
+++ edx_sga/static/js/src/edx_sga.js
@@ -334,8 +334,8 @@ function StaffGradedAssignmentXBlock(runtime, element) {
                 }
             });
 
-            form.find(".annotatedFileDelete").on("click", annotated[this.value], function(metadata) {
-                var url = deleteAnnotationFileUrl + "/" + metadata.sha1
+            form.find(".annotatedFileDelete").on("click", annotated[this.value].sha1, function(hash) {
+                var url = deleteAnnotationFileUrl + "/" + hash
                     + '?module_id=' + row.data("module_id");
                 $.get(url).success(function ( data ) {
                         renderStaffGrading(data);                    
