389a73b9c4c902cc8586579261104c7e0c487132
Benjamin Snively <wastedelectrons@outlook.com>
2014-11-24 15:00:00 -0500

    merging both uploads

--- edx_sga/static/js/src/edx_sga.js
+++ edx_sga/static/js/src/edx_sga.js
@@ -409,8 +409,8 @@ function StaffGradedAssignmentXBlock(runtime, element)
             fileListDiv.html(filelistTemplate(fileState));
 
             fileListDiv.find(".fileDownload").each(function(index) {
-                $(this).attr("href", 
-                    fileState.downloadUrl(fileState.filelist[$(this).val].sha1))
+                url = fileState.downloadUrl(fileState.filelist[$(this).val].sha1);
+                $(this).attr("href", url);
             });
 
             fileListDiv.find(".fileDelete").on("click", function() {
