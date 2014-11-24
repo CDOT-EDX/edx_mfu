2d56959bbf1da004950117384d1788ce8c4ecda3
Benjamin Snively <wastedelectrons@outlook.com>
2014-11-24 13:33:24 -0500

    merging both uploads

--- edx_sga/file_annotation_mixin.py
+++ edx_sga/file_annotation_mixin.py
@@ -52,7 +52,7 @@ class FileAnnotationMixin(XBlockMixin):
             "sha1":      uploaded.sha1, 
             "filename":  uploaded.filename,
             "timestamp": uploaded.timestamp
-        )
+        }
 
 	@XBlock.handler
 	def student_download_annotated(self, request, suffix=''):
